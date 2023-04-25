a601cfbf614db3ba2f5748835e816ba0df7a96f3
Rowanti <90836552+Rowanti@users.noreply.github.com>
2023-04-25 17:27:51 +0200

    Delete Jaar1/Programmeren_3_java directory

--- Jaar1/Programmeren_3_java/Kade.java
+++
@@ -1,9 +0,0 @@
-/**
- * Herkansing einopdracht programmeren 3
- * Rowan van der Zanden - 1027332
- */
-import java.util.ArrayList;
-public class Kade {
-    public int vrachtwagenCount = 10;
-    public ArrayList<Container> kadeList = new ArrayList<Container>();
-}
